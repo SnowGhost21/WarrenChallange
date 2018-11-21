f88bec3b6c9dd5d66f29db428d45030bde771ae5
Diego <domdiegobc@gmail.com>
2018-11-21 09:10:11 -0200

    -Updated call methods and removed unnecessary information -Created fixtures for tests

--- app/src/main/java/com/diegocunha/warrenchat/view/home/HomeViewModel.kt
+++ app/src/main/java/com/diegocunha/warrenchat/view/home/HomeViewModel.kt
@@ -3,47 +3,68 @@ package com.diegocunha.warrenchat.view.home
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.diegocunha.warrenchat.model.data.Answer
 import com.diegocunha.warrenchat.model.data.BodyMessage
 import com.diegocunha.warrenchat.model.data.Message
 import com.diegocunha.warrenchat.model.repository.MessageRepository
-import iwsbrazil.io.artmuseum.extensions.asLiveData
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
 
 class HomeViewModel constructor(private val repository: MessageRepository) : ViewModel() {
 
     private val _answers = MutableLiveData<Message>()
     val answers: LiveData<Message> = _answers
+    private val _error = MutableLiveData<String>()
+    val error: LiveData<String> = _error
     private val answersHistoric = HashMap<String, Any>()
     private var lastReceivedMessageId: String = ""
+    private var initialMessageDisposable: Disposable? = null
+    private var sendMessageDisposable: Disposable? = null
 
     init {
         initChat()
     }
 
+    override fun onCleared() {
+        super.onCleared()
+        initialMessageDisposable?.dispose()
+        sendMessageDisposable?.dispose()
+    }
+
     private fun initChat() {
-        repository.getInitialMessage()
-            .doOnSuccess { response ->
-                setLastReceivedId(response.id)
-                response.messages.forEach {
-                    addMessage(it, Message.BOT)
-                }
-            }.asLiveData()
+        initialMessageDisposable = repository.getInitialMessage()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({ configureMessage(it, Message.BOT) }, { _error.postValue(it.message) })
+    }
+
+    private fun configureMessage(answer: Answer, sent: String) {
+        setLastReceivedId(answer.id)
+        answer.messages.forEach {
+            addMessage(it, sent)
+        }
     }
 
     fun sendMessage(message: String) {
         val userMessage = Message("string", message, Message.USER)
         addMessage(userMessage)
-
-        answersHistoric[lastReceivedMessageId] = message
+        updateHistoricy(message)
         val bodyMessage = BodyMessage(lastReceivedMessageId, answersHistoric)
-        repository.sendMessage(bodyMessage)
-            .doOnSuccess {
-                setLastReceivedId(it.id)
-                it.messages.forEach { message ->
-                    addMessage(message, Message.BOT)
-                }
-            }.asLiveData()
+
+        sendMessageDisposable = repository.sendMessage(bodyMessage)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({ configureMessage(it, Message.BOT) }, { _error.postValue(it.message) })
     }
 
+    private fun setLastReceivedId(id: String) {
+        lastReceivedMessageId = id
+    }
+
+    private fun updateHistoricy(message: String) {
+        answersHistoric[lastReceivedMessageId] = message
+    }
 
     private fun addMessage(message: Message, sent: String) {
         message.sent = sent
@@ -54,11 +75,6 @@ class HomeViewModel constructor(private val repository: MessageRepository) : Vie
     private fun addMessage(message: Message) {
         val cleanedMessage = cleanMessage(message)
         _answers.postValue(cleanedMessage)
-    }
-
-
-    private fun setLastReceivedId(id: String) {
-        lastReceivedMessageId = id
     }
 
     private fun cleanMessage(message: Message): Message {
--- app/src/test/java/com/diegocunha/warrenchat/model/fixture/BodyFixture.kt
+++ app/src/test/java/com/diegocunha/warrenchat/model/fixture/BodyFixture.kt
@@ -1,2 +1,18 @@
 package com.diegocunha.warrenchat.model.fixture
 
+import com.diegocunha.warrenchat.model.data.Answer
+import com.diegocunha.warrenchat.model.data.BodyMessage
+import com.diegocunha.warrenchat.model.data.Message
+
+val createHistory = HashMap<String, Any>()
+
+val createBody = BodyMessage("abc", createHistory)
+
+val createMessageUser = Message("string", "Hello", "user")
+
+val createMessageBot = Message("string", "Hello my friend", "bot")
+
+val createUserAnswer = Answer("abc", listOf(createMessageUser))
+
+val createBotAnswer = Answer("def", listOf(createMessageBot))
+
